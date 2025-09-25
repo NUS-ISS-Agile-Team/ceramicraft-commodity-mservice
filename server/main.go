b8a0b5fd7347105eaf9da2aba85f3a27e7cb74bf
YangZhicheng <yangzhicheng@YangZhichengdeMacBook-Air.local>
2025-09-25 21:01:12 +0800

    chore: productService init

---
+++ server/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/NUS-ISS-Agile-Team/ceramicraft-commodity-mservice/server/config"
+	"github.com/NUS-ISS-Agile-Team/ceramicraft-commodity-mservice/server/log"
+	"github.com/NUS-ISS-Agile-Team/ceramicraft-commodity-mservice/server/repository"
+)
+
+var sigCh = make(chan os.Signal, 1)
+
+func main() {
+	config.Init()
+	log.InitLogger()
+	repository.Init()
+	go grpc.Init(sigCh)
+	go http.Init(sigCh)
+	// listen terminage signal
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-sigCh // Block until signal is received
+	log.Logger.Infof("Received signal: %v, shutting down...", sig)
+}
---
+++ server/repository/model/product.go
@@ -0,0 +1,18 @@
+package model
+
+import "gorm.io/gorm"
+
+type Product struct {
+	gorm.Model
+	ProductId int64  `gorm:"uniqueIndex;not null"`
+	Name      string `gorm:"not null"`
+	Price     int64  `gorm:"not null"`
+	Desc      string `gorm:"type:text"`
+	Stock     int64  `gorm:"not null"`
+	PicInfo   string `gorm:"type:text"`
+	Status    int32  `gorm:"not null"` // 0: 未上架, 1: 已上架
+}
+
+func (Product) TableName() string {
+	return "products"
+}
